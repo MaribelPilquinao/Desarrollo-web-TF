63e6d404b7a6c2d1383365f80b8a5bfe7c05b227
Dylan Chilet <dylanchiletrock@hotmail.com>
2026-09-13 19:27:04 -0500

    Variables y conexion de formulario del checkout con Angular

--- src/app/pages/checkout/checkout.ts
+++ src/app/pages/checkout/checkout.ts
@@ -1,21 +1,48 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-checkout',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './checkout.html',
-  styleUrl: './checkout.css',
+  styleUrl: './checkout.css'
 })
 export class Checkout {
+
+  // Datos personales
+  nombres = '';
+  apellidos = '';
+  correo = '';
+  telefono = '';
+
+  // Dirección
+  departamento = '';
+  distrito = '';
+  direccion = '';
+  referencia = '';
+
+  // Método de pago
+  metodoPago = '';
+
+  // Datos temporales del producto
   producto = 'Laptop HP';
   precio = 2500;
-  cantidad = 2;
-  envio=15;
+  cantidad = 1;
+  envio = 15;
 
   subtotal = this.precio * this.cantidad;
   total = this.subtotal + this.envio;
 
   confirmarCompra() {
-    alert('Compra confirmada :)');
+    console.log('Nombres:', this.nombres);
+    console.log('Apellidos:', this.apellidos);
+    console.log('Correo:', this.correo);
+    console.log('Teléfono:', this.telefono);
+    console.log('Departamento:', this.departamento);
+    console.log('Distrito:', this.distrito);
+    console.log('Dirección:', this.direccion);
+    console.log('Método de pago:', this.metodoPago);
+
+    alert('Datos registrados correctamente');
   }
 }
