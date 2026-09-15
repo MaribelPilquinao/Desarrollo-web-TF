f0c1e9cf889c9d1a2cd14e0f84c5b28f7f635985
MaribelPilquinao <maribel.pilquinaopm@gmail.com>
2026-09-14 20:16:31 -0500

    Se agregó las funcionalidades de carrito y favoritos al proyecto, el footer se ha posicionado al final de la pagina para evitar que se vea en medio de la pagina y que esté estandar con todos las paginas del proyecto. Se linkeo las rutas en el archivo app.html para navegar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
 import { Home } from './pages/home/home';
+import { Cart } from './pages/cart/cart';
+import { Favorites } from './pages/favorites/favorites';
 
 export const routes: Routes = [
-    {path: "home", component: Home}
+    {path: "", redirectTo: "home", pathMatch: "full"},
+    {path: "home", component: Home},
+    {path: "cart", component: Cart},
+    {path: "favorites", component: Favorites}
 ];
