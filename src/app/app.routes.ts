bd1e993184e17f4dad16c6ee4d84968ec6dc904a
RosaSalinasC <sonam.rosa@gmail.com>
2026-09-20 01:11:56 -0500

    feat: crear componente producto detalle y configurar ruta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,11 +5,14 @@ import { Confirmacion } from './pages/confirmacion/confirmacion';
 import { Favorites } from './pages/favorites/favorites';
 import { Home } from './pages/home/home';
 import { MiCuenta } from './pages/mi-cuenta/mi-cuenta';
+import { ProductoDetalle } from './pages/producto-detalle/producto-detalle';
+
 
 export const routes: Routes = [
     { path: "", redirectTo: "home", pathMatch: "full" },
     { path: "home", component: Home },
     { path: "cart", component: Cart },
+    { path: "producto/:id", component: ProductoDetalle },
     { path: "favorites", component: Favorites },
     { path: 'checkout', component: Checkout },
     { path: 'confirmacion', component: Confirmacion },
