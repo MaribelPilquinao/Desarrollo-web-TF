6624aa9c9f399d74f793f8328873437c903fe97a
RosaSalinasC <sonam.rosa@gmail.com>
2026-09-20 10:05:57 -0500

    feat: implementar cambio de imagen en galeria

--- src/app/pages/producto-detalle/producto-detalle.ts
+++ src/app/pages/producto-detalle/producto-detalle.ts
@@ -7,4 +7,36 @@ import { RouterLink } from '@angular/router';
   templateUrl: './producto-detalle.html',
   styleUrl: './producto-detalle.css'
 })
-export class ProductoDetalle {}
+export class ProductoDetalle {
+
+  imagenPrincipal =
+    'https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1200';
+
+  imagenes = [
+    {
+      miniatura:
+        'https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=400',
+      completa:
+        'https://images.pexels.com/photos/18105/pexels-photo.jpg?auto=compress&cs=tinysrgb&w=1200',
+      alt: 'Vista 1'
+    },
+    {
+      miniatura:
+        'https://images.pexels.com/photos/374074/pexels-photo-374074.jpeg?auto=compress&cs=tinysrgb&w=400',
+      completa:
+        'https://images.pexels.com/photos/374074/pexels-photo-374074.jpeg?auto=compress&cs=tinysrgb&w=1200',
+      alt: 'Vista 2'
+    },
+    {
+      miniatura:
+        'https://images.pexels.com/photos/177598/pexels-photo-177598.jpeg?auto=compress&cs=tinysrgb&w=400',
+      completa:
+        'https://images.pexels.com/photos/177598/pexels-photo-177598.jpeg?auto=compress&cs=tinysrgb&w=1200',
+      alt: 'Vista 3'
+    }
+  ];
+
+  cambiarImagen(imagen: string): void {
+    this.imagenPrincipal = imagen;
+  }
+}
