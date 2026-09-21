56a55816225c4480a4426168eae74ef98e43e587
MaribelPilquinao <maribel.pilquinaopm@gmail.com>
2026-09-20 22:02:53 -0500

    agregar funcionalidad de favorito en cada card, mejora en footer

--- src/app/pages/favorites/favorites.ts
+++ src/app/pages/favorites/favorites.ts
@@ -1,9 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-favorites',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './favorites.html',
   styleUrl: './favorites.css',
 })
-export class Favorites {}
+export class Favorites implements OnInit {
+
+  private readonly FAV_KEY = 'malvitec_favorites';
+
+  favoritos: {
+    id: string;
+    title: string;
+    price: number;
+    img: string;
+  }[] = [];
+
+  ngOnInit(): void {
+    this.cargarFavoritos();
+  }
+
+  cargarFavoritos(): void {
+    if (typeof localStorage === 'undefined') {
+      return;
+    }
+
+    const datos = localStorage.getItem(this.FAV_KEY);
+
+    if (!datos) {
+      this.favoritos = [];
+      return;
+    }
+
+    try {
+      const favoritosGuardados = JSON.parse(datos);
+
+      this.favoritos = Array.isArray(favoritosGuardados)
+        ? favoritosGuardados
+        : [];
+
+    } catch {
+      this.favoritos = [];
+    }
+  }
+
+  eliminarFavorito(id: string): void {
+    this.favoritos = this.favoritos.filter(
+      producto => producto.id !== id
+    );
+
+    localStorage.setItem(
+      this.FAV_KEY,
+      JSON.stringify(this.favoritos)
+    );
+  }
+}
